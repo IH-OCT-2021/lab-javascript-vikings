7f437c256827948b79454b7943ff498fdac0e3a2
Connor McNeil <cnnrmcnl@gmail.com>
2021-11-04 11:34:44 -0400

    connor

--- src/viking.js
+++ src/viking.js
@@ -71,10 +71,55 @@ class War {
     }
     return battleText
   }
+  showStatus(){
+    if(this.saxonArmy.length <= 0){
+      
+      clearInterval(int)
+      return 'Vikings have won the war of the century!'
+    }
+    if(this.vikingArmy.length <= 0){
+      
+      clearInterval(int)
+      return 'Saxons have fought for their lives and survived another day...'
+      
+    }
+    else {
+      return 'Vikings and Saxons are still in the thick of battle.'
+  }
+  }
 }
 
+let battle = new War()
+let saxon1 = new Saxon(100, 50)
+let saxon2 = new Saxon(99, 50)
+let saxon3 = new Saxon(90, 50)
+let saxon4 = new Saxon(89, 50)
+let saxon5 = new Saxon(67, 50)
 
+let viking1 = new Viking('Thor', 99999, 40)
+let viking2 = new Viking('Odin', 99999, 40)
+let viking3 = new Viking('Norse', 99999, 40)
+let viking4 = new Viking('Loki', 99999, 40)
+let viking5 = new Viking('Ragnarok', 99999, 40)
 
+battle.addSaxon(saxon1)
+battle.addSaxon(saxon2)
+battle.addSaxon(saxon3)
+battle.addSaxon(saxon4)
+battle.addSaxon(saxon5)
+
+battle.addViking(viking1)
+battle.addViking(viking2)
+battle.addViking(viking3)
+battle.addViking(viking4)
+battle.addViking(viking5)
+
+let int = setInterval(() => {
+  console.log("Swords clink")
+  console.log(battle.saxonAttack())
+  console.log(battle.vikingAttack())
+  console.log(battle.showStatus())
+},100)
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
